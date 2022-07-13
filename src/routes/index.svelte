500c3992914c855a82c5ddfd95d0b1ee867b6b4f
jordan190 <jordanlavatai@gmail.com>
2022-07-13 13:15:11 -0700

    transitioning to page/endpoint repo structure for charts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,24 +2,27 @@
 	import { session } from '$app/stores';
 	import supabase from '$lib/supabase';
 	import Auth from '$lib/Auth.svelte';
+	import Trading from '$lib/Trading.svelte';
 
 	session.subscribe((v) => {
 		console.log('session fired', v);
 	});
 
 	const authSession = supabase.auth.session();
-	if (authSession) session.set({
-        email: authSession.user?.email
-    });
+	if (authSession)
+		session.set({
+			email: authSession.user?.email
+		});
 
 	supabase.auth.onAuthStateChange((_, authSession) => {
-		session.set({ email: authSession?.user?.email});
+		session.set({ email: authSession?.user?.email });
 	});
 </script>
 
 <h1>Robbin for the good of humanity</h1>
 {#if $session}
 	{$session.email}
+	<Trading />
 {:else}
 	<Auth />
 {/if}
