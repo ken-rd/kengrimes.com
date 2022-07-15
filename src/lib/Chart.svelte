551360c87806146677e8857e5c0932ea1dc6a8db
jordan190 <jordanlavatai@gmail.com>
2022-07-14 19:12:28 -0700

    rendering asset symbol with data points. i bow to the ai gods

---
+++ src/lib/Chart.svelte
@@ -0,0 +1,71 @@
+<script>
+	import Line from 'svelte-chartjs/src/Line.svelte';
+
+	export let symbol = '';
+	let assetData = null;
+	/**
+	 * @type {{ labels: string[]; datasets: { label: any; fill: boolean; data: number[]; }[]; } | null}
+	 */
+	let chartData = null;
+
+	async function populateChartData() {
+		assetData = await getAssetData();
+		chartData = assetToChartJS(assetData);
+	}
+
+	/* Returns a promise with the asset data from yahoo finance */
+	function getAssetData() {
+		return fetch(`/assets/${symbol}`)
+			.then((res) => res.json())
+			.then(({ chartData }) => {
+				assetData = chartData;
+				return assetData;
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	}
+
+	/* convert array of datetime stamps to months */
+	function convertArrayToMonths(array) {
+		return array.map((date) => {
+			
+		});
+	}
+
+	/**
+	 * @param {*} assetData
+	 */
+	function assetToChartJS(assetData) {
+		console.log('converting asset to chart');
+		console.log(assetData);
+		const chartJSFormatted = {
+			labels: assetData.quotes.map((/** @type {{ date: any; }} */ quote) => new Date(quote.date).toLocaleDateString('en-US', {
+				month: 'short'
+			})),
+			datasets: [
+				{
+					label: assetData.meta.symbol,
+					fill: false,
+					data: assetData.quotes.map((/** @type {{ close: any; }} */ quote) => quote.close)
+				}
+			]
+		};
+		return chartJSFormatted;
+	}
+</script>
+
+<!--component to fetch, format, load into generic chart implementation. 
+    But chart implementations arent generic so, what would be the contract
+	  for interacting with -->
+
+<div>
+	{#if symbol}
+		<button class="button-41" on:click={populateChartData}> fetch stonk chart data </button>
+	{/if}
+	{#await chartData then}
+		<Line data={chartData} options={{ responsive: true }} />
+	{:catch}
+		<p>waiting for stonk data</p>
+	{/await}
+</div>
--- src/lib/Trading.svelte
+++ src/lib/Trading.svelte
@@ -1,64 +1,18 @@
 <script>
 	import Search from 'svelte-search';
-	import Chart from '../routes/charts/[symbol].svelte'
-
-	let value = '';
+	import Chart from './Chart.svelte';
+	
+	let symbol = '';
 	let hideLabel = true;
-	$: symbol = value;
-	/**
-	 * @type {*}
-	 */
-	let symbolData;
-
-	function logSymbol() {
-		console.log(symbol);
-	}
 </script>
 
 <div>
-	<Search bind:value debounce={100} {hideLabel} />
-	{#if value}
-		<button class="button-41" on:click={logSymbol}> fetch stonk chart data </button>
+	<Search bind:value={symbol} {hideLabel} />
+	{#if symbol}
+		<Chart {symbol} />
 	{/if}
-	{#await symbolData}
-		<p>choose a stonk</p>
-	{:then}
-		<Chart chartData={symbolData} />
-	{:catch}
-		alert('error getting chart data')
-	{/await}
 </div>
 
 <style>
-	/* CSS */
-	.button-41 {
-		background-color: initial;
-		background-image: linear-gradient(-180deg, #00d775, #00bd68);
-		border-radius: 5px;
-		box-shadow: rgba(0, 0, 0, 0.1) 0 2px 4px;
-		color: #ffffff;
-		cursor: pointer;
-		display: inline-block;
-		font-family: Inter, -apple-system, system-ui, Roboto, 'Helvetica Neue', Arial, sans-serif;
-		height: 44px;
-		line-height: 44px;
-		outline: 0;
-		overflow: hidden;
-		padding: 0 20px;
-		pointer-events: auto;
-		position: relative;
-		text-align: center;
-		touch-action: manipulation;
-		user-select: none;
-		-webkit-user-select: none;
-		vertical-align: top;
-		white-space: nowrap;
-		width: 100%;
-		z-index: 9;
-		border: 0;
-	}
-
-	.button-41:hover {
-		background: #00bd68;
-	}
+	
 </style>
